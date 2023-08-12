4543f649acd5867b938520ca5584f13cdd214f8e
Maurice809 <maurice809@hotmail.com>
2023-08-12 08:46:06 +0200

    42 Lausanne | SANA | 12/08/2023  8:46:06,51

---
+++ Listing_2.1/Livre/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream.h>
+
+void AfiicheTauxTVA()
+{
+	float TauxTVA = 20.6;
+	cout << "TVA : " << TauxTVA;
+}
+
+int main(void)
+{
+	AfiicheTauxTVA();
+	return 0;
+}
---
+++ Listing_2.1/New/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void AfiicheTauxTVA()
+{
+	float TauxTVA = 20.6;
+	cout << "TVA : " << TauxTVA;
+}
+
+int main(void)
+{
+	AfiicheTauxTVA();
+	return 0;
+}
