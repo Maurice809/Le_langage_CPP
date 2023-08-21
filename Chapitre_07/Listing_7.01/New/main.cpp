b8506926b19237d7e907978c63802828bd303f39
Maurice809 <maurice809@hotmail.com>
2023-08-21 15:16:56 +0200

    42 Lausanne | SANA | 21/08/2023 15:16:56,54

--- Chapitre_07/Listing_7.01/New/main.cpp
+++ Chapitre_07/Listing_7.01/New/main.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
+class Micro
+{
+	public:
+	char Reference[20+1];
+	char Marque[20+1];
+	char Processeur[20+1];
+	int Disque;
+
+	Micro(const char *r, const char *m, const char *p, int d);
+
+	void Affiche();
+};
+
+Micro::Micro(const char *r, const char *m, const char *p, int d)
+{
+	strcpy(Reference, r);
+	strcpy(Marque, m);
+	strcpy(Processeur, p);
+	Disque = d;
+}
+
+void Micro::Affiche()
+{
+	cout << endl;
+	cout << " Ref : " << Reference;
+	cout << " Marque : " << Marque;
+	cout << " Processeur : " << Processeur;
+	cout << " Disque : " << Disque;
+}
+
 int main()
 {
+	Micro Mic("X15", "ELX", "PIII", 400);
+	Mic.Affiche();
+
+	cout << endl;
+
+	Micro *pMic;
+	pMic = new Micro("X16", "PH", "DX4 - 100", 200);
+	pMic->Affiche();
+	delete pMic;
+	cout << endl;
+
 	return 0;
 }
 
