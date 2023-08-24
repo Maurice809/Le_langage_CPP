e025a89318026d145db5fc6b10ccf9f49d706403
Maurice809 <maurice809@hotmail.com>
2023-08-24 09:30:37 +0200

    42 Lausanne | MOMO | 24.08.2023  9:30:37,66

---
+++ Chapitre_08/Listing_8.04/Livre/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream.h>
+#include <conio.h>
+
+class Materiel
+{
+	public:
+		Materiel();
+		~Materiel();
+};
+
+class Micro : public Materiel
+{
+	public:
+		Micro();
+		~Micro();
+};
+
+Materiel::Materiel()
+{
+	cout << " Constructeur Materiel : " << this << endl;
+}
+
+Materiel::~Materiel()
+{
+	cout << " Destructeur Materiel  : " << this << endl;
+}
+
+Micro::Micro()
+{
+	cout << " Constructeur Micro    : " << this << endl;
+}
+
+Micro::~Micro()
+{
+	cout << " Destructeur Micro     : " << this << endl;
+}
+
+int main()
+{
+	cout << endl;
+	cout << "CREATION D'OBJET DE TYPE MATERIEL" << endl;
+	Materiel *pMat;
+	pMat = new Materiel;
+	cout << "DESTRUCTEUR DE L'OBJET DE TYPE MATERIEL" << endl;
+	delete pMat;
+	cout << endl;
+	cout << "CREATION D'OBJET DE TYPE MICRO" << endl;
+	Micro *pMic;
+	pMic = new Micro;
+	cout << endl;
+	cout << "DESTRUCTION DE L'OBJET DE TYPE MICRO" << endl;
+	delete pMic;
+	cout << endl;
+	getch();
+
+    return 0;
+}
