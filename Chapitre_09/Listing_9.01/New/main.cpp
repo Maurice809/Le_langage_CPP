2c3a27f250a59139620e0c74d3e1e19417e0f090
Maurice809 <maurice809@hotmail.com>
2023-08-25 12:03:11 +0200

    42 Lausanne | MOMO | 25.08.2023 12:03:10,92

---
+++ Chapitre_09/Listing_9.01/New/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Materiel
+{
+    protected:
+        char Reference[20+1];
+        char Marques[20+1];
+    public:
+        Materiel(const char *r, const char *m);
+        void Affiche();
+};
+
+class Micro : public Materiel
+{
+    protected:
+        char Processeur[20+1];
+        int Disque;
+    public:
+        Micro(const char *r, const char *m, const char *p, int d);
+        void Affiche();
+};
+
+Materiel::Materiel(const char *r, const char *m)
+{
+    strcpy(Reference, r);
+    strcpy(Marques, m);
+}
+
+void Materiel::Affiche()
+{
+    cout << " Ref : " << Reference;
+    cout << " Marque : " << Marques;
+    cout << endl;
+}
+
+Micro::Micro(const char *r, const char *m, const char *p, int d) 
+      :Materiel(r,m)
+{
+    strcpy(Processeur, p);
+    Disque = d;
+}
+
+void Micro::Affiche()
+{
+    cout << " Ref : " << Reference;
+    cout << " Marque : " << Marques;
+    cout << " Processeur :" << Processeur;
+    cout << " Disque : " << Disque;
+    cout << endl;
+}
+
+int main()
+{
+    Materiel *pMat;
+
+    cout << endl;
+    pMat = new Materiel("X01", "XX");
+    pMat->Affiche();
+    delete pMat;
+
+    pMat = new Micro("X16", "PH", "DX4-100", 200);
+    pMat->Affiche();
+    delete pMat;
+
+    return 0;
+}
---
+++ Chapitre_09/Listing_9.01/Readme.md
@@ -0,0 +1,1 @@
+Utilisation d'un pointeur vers la classe de base
